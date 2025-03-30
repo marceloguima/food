76ab75ff6f0f4f734139c2dd403d9b16e1de3472
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-03-30 19:47:04 -0400

    feito alogica do carrinho, está adicionando e subtraindo

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,86 @@
-const  btnMostraCart = document.getElementById("mostra-cart")
-const cart = document.getElementById("cart")
+const btnMostraCart = document.getElementById("mostra-cart");
+const divCart = document.querySelector(".cart");
+const ulcart = document.getElementById("cart-lista");
+const Total = document.getElementById("total");
+const QuantidadeCart = document.getElementById("quantidade");
 
-function mostrarCart(){
-    if(cart.style.display === "flex"){
-        cart.style.display = "none"
-    }else{
-        cart.style.display = "flex"
+let itensCart = [];
+
+function mostrarCart() {
+    if (divCart.style.display === "flex") {
+        divCart.style.display = "none";
+    } else {
+        divCart.style.display = "flex";
+    }
+}
+
+function adicionarNoCarrinho(codigo, nome, preco, imagem) {
+    let itemExiste = itensCart.find((item) => item.codigo === codigo);
+
+    if (itemExiste) {
+        itemExiste.quantidade++;
+    } else {
+        itensCart.push({
+            codigo: codigo,
+            nome: nome,
+            preco: preco,
+            quantidade: 1,
+            imagem: imagem,
+        });
+    }
+
+    atualizarCart();
+}
+
+function atualizarCart() {
+    ulcart.innerHTML = "";
+    spanTotal = document.getElementById("total");
+    let total = 0;
+    let quantidadeTotal = 0;
+
+    itensCart.forEach((item) => {
+        let itensCarrinho = document.createElement("li");
+        itensCarrinho.classList.add("item");
+        itensCarrinho.innerHTML = `<img src="${item.imagem}" alt="${
+            item.nome
+        }" />
+                           <p>${item.nome}</p>
+                              <div class="caixa-botao">
+                            <button onclick="aumentarQuantidade('${
+                                item.codigo
+                            }')"><i class="fa-solid fa-plus"></i></button>
+                               <p><span id="quant-do-item">${
+                                   item.quantidade
+                               }</span></p>
+                            <button onclick="diminuirQuantidade('${
+                                item.codigo
+                            }')"><i class="fa-solid fa-minus"></i></button>
+                             </div>
+                            <p>R$ <span>${(
+                                item.preco * item.quantidade
+                            ).toFixed(2)}</span></p>`;
+
+        ulcart.append(itensCarrinho);
+        total += item.preco * item.quantidade;
+        quantidadeTotal += item.quantidade;
+    });
+
+    spanTotal.textContent = `${total.toFixed(2)}`;
+    QuantidadeCart.textContent = quantidadeTotal;
+}
+
+function aumentarQuantidade(codigo) {
+    let item = itensCart.find((item) => item.codigo === codigo);
+    if (item) {
+        item.quantidade++;
+        atualizarCart();
+    }
+}
+
+function diminuirQuantidade(codigo) {
+    let item = itensCart.find((item) => item.codigo === codigo);
+    if (item && item.quantidade > 1) {
+        item.quantidade--;
+        atualizarCart();
     }
 }
